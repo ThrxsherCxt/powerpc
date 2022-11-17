e627ed2ef5606898b280a898a5abe4595bb392e0
Mario Alberto Rivera Aguilar <thrxshercxt@gmail.com>
2022-11-16 18:24:59 -0600

    Funcionamiento del routing y de MVC correcto.

---
+++ install/config.php
@@ -0,0 +1,13 @@
+<?php
+// Parámetros DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'powerpc');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)).'/php');
+// URL Root
+define('URLROOT', 'http://localhost/powerpc');
+// Nombre del sitio
+define('SITENAME', 'PowerPC');
---
+++ php/views/pages/index.php
@@ -0,0 +1,3 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<h1>Hola Mundo</h1>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
